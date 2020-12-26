7af78827000ef01c057eef1da3a4d46a80c54692
Hendiadyoin1 <leon2002.la@gmail.com>
2020-12-26 17:43:10 +0100

    try to fix operators for complex in win

--- src/_complex.h
+++ src/_complex.h
@@ -4,6 +4,17 @@
 
 #if defined(_MSC_VER) //Windows
     #define complex_t _Dcomplex
+
+    complex_t operator *(const complex_t& a, const complex_t& b){
+        return _Cmulcc(a,b);
+    }
+    complex_t operator *(const complex_t& a, const double& b){
+        return _Cmulcr(a,b);
+    }
+    complex_t operator *(const double& b, const complex_t& a,){
+        return _Cmulcr(a,b);
+    }
+
 #elif defined(__GNUC__) // GCC
     #define complex_t double complex
 #endif
