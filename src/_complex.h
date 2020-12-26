67aa8abd9a31af4e92c3822914860d4341f3c5b8
Hendiadyoin1 <leon2002.la@gmail.com>
2020-12-26 17:26:31 +0100

    revert win complex type

--- src/_complex.h
+++ src/_complex.h
@@ -3,7 +3,7 @@
 #include <complex.h>
 
 #if defined(_MSC_VER) //Windows
-    #define complex_t complex<double>
+    #define complex_t _Dcomplex
 #elif defined(__GNUC__) // GCC
     #define complex_t double complex
 #endif
