4484dd1c8fdb49655ee3d3fdfbc47231ed666780
Hendiadyoin1 <leon2002.la@gmail.com>
2020-12-26 20:04:01 +0100

    fixed some assignments

--- src/_complex.h
+++ src/_complex.h
@@ -4,7 +4,8 @@
 
 #if defined(_MSC_VER) //Windows
     #define complex_t _Dcomplex
-    #define Complex_t(reel, imag) _DCOMPLEX_(reel,imag)
+    #define Complex_t(real, imag) _DCOMPLEX_(real,imag)
+
 #elif defined(__GNUC__) // GCC
     #define complex_t double complex
     #define Complex_t(reel, complex) (reel + complex * _Complex_I)
