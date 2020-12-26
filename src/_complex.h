2cbafcd69dcc1572584afe0a075c81e39114f010
Hendiadyoin1 <leon2002.la@gmail.com>
2020-12-26 17:21:27 +0100

    changed complex type name for windows

--- src/_complex.h
+++ src/_complex.h
@@ -3,7 +3,7 @@
 #include <complex.h>
 
 #if defined(_MSC_VER) //Windows
-    #define complex_t _Dcomplex
+    #define complex_t complex<double>
 #elif defined(__GNUC__) // GCC
     #define complex_t double complex
 #endif
