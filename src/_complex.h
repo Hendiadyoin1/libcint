21a66e9d2384d84035358c81d76e98cc4e93b32c
Hendiadyoin1 <leon2002.la@gmail.com>
2020-12-26 17:58:13 +0100

    attempt to fix Comlex creation through constructor

--- src/_complex.h
+++ src/_complex.h
@@ -4,17 +4,8 @@
 
 #if defined(_MSC_VER) //Windows
     #define complex_t _Dcomplex
-
-    complex_t operator* (const complex_t& a, const complex_t& b){
-        return _Cmulcc(a,b);
-    }
-    complex_t operator* (const complex_t& a, const double& b){
-        return _Cmulcr(a,b);
-    }
-    complex_t operator* (const double& b, const complex_t& a,){
-        return _Cmulcr(a,b);
-    }
-
+    #define complex_t(reel, imag) _Dcomplex(reel,imag)
 #elif defined(__GNUC__) // GCC
     #define complex_t double complex
+    #define complex_t(reel, complex) (reel + complex * _Complex_I)
 #endif
